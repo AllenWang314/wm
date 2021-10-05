f8c2a4316c65e20b4b6f460331c5272b3cf0953c
Allen Wang <abmathcounts@gmail.com>
2021-10-05 01:32:25 -0400

    adding new settings

--- backend/settings.py
+++ backend/settings.py
@@ -60,6 +60,10 @@ CORS_ORIGIN_WHITELIST = (
 
 ROOT_URLCONF = 'backend.urls'
 
+TEMPLATE_DIRS = (
+    'build/',
+)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
