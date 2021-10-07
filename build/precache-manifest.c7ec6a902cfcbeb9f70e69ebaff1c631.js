036b406a9ea0bc4b2b5f2f31b637113859b8bc4c
Allen Wang <abmathcounts@gmail.com>
2021-10-07 03:53:54 -0400

    adding final js files

---
+++ build/precache-manifest.c7ec6a902cfcbeb9f70e69ebaff1c631.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41fdb9ce05b84cf4f17940e3af80c665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8f632e547f561d4c318",
+    "url": "/static/css/main.92dd3312.chunk.css"
+  },
+  {
+    "revision": "237db3300a3e9d1c46c9",
+    "url": "/static/js/2.28c230d8.chunk.js"
+  },
+  {
+    "revision": "8d8f38a82785c6395703cc0023a7264d",
+    "url": "/static/js/2.28c230d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f632e547f561d4c318",
+    "url": "/static/js/main.886075a5.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  },
+  {
+    "revision": "d221381cc0ed7fb7a2afbf59391325bf",
+    "url": "/static/media/copy_link_demo.d221381c.gif"
+  },
+  {
+    "revision": "2d7578f4cd3d6493648a8c685d14dd24",
+    "url": "/static/media/select_times_demo.2d7578f4.gif"
+  },
+  {
+    "revision": "c7e50dabf51a088dd70b8056e6393b23",
+    "url": "/static/media/sign_in_demo.c7e50dab.gif"
+  },
+  {
+    "revision": "d1a28c9855a3a70962b84dfb9522cbbe",
+    "url": "/static/media/view_availabilities_demo.d1a28c98.gif"
+  }
+]);
